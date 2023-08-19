3196c2c8460c7248aa42513507a058742eb36545
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-08-19 14:37:25 +0500

    Other files also commited

---
+++ OddEven/OddEven/OddEvenPrime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddEven
+{
+    public class OddEvenPrime
+    {
+        private static readonly string ODD = "ODD";
+        private static readonly string EVEN = "EVEN";
+
+
+        public void Run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(CheckOddEven(i));
+            }
+        }
+        
+        public static string CheckOddEven(int num)
+        {
+            if (num < 1 || num > 100)
+                throw new ArgumentException("The number should be between 1-100 ");
+
+            if (num % 2 == 0)
+                return EVEN;
+
+            if (num % 3 == 0)
+                return ODD;
+
+            return num.ToString();
+        }
+    }
+}
---
+++ OddEven/OddEven/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OddEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var oddEven = new OddEvenPrime();
+            oddEven.Run();
+        }
+    }
+}
